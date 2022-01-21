1d844b75fac5267113a0f853401cc9cea39ea06a
Steve Tong <stevetong53@yahoo.com>
2022-01-21 00:49:13 -0800

    got status bar for android positioning where I want it, IOS device working status bar

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{ color: "white" }}>Hello World!</Text>
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
