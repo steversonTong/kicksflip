5c0c87caede12eca738b05c2a79a06fb2a94f663
Steve Tong <stevetong53@yahoo.com>
2022-01-22 21:28:12 -0800

    added animation but the animation for ksflogo not working

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -20,6 +20,8 @@ const SplashScreen = ({ navigation }) => {
       <StatusBar backgroundColor="#009387" barStyle="light-content" />
       <View style={styles.header}>
         <Animatable.Image
+          animation="bounceIn"
+          duraton="1500"
           source={require("../assets/kicksfliplogo.png")}
           style={styles.logo}
           resizeMode="stretch"
