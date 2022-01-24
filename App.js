c63e8587b81a64703c11da486376ac22f09cfa1d
Steve Tong <stevetong53@yahoo.com>
2022-01-23 19:03:09 -0800

    auth context created

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainTabScreen from "./screens/MainTabScreen";
@@ -10,6 +10,28 @@ const App = () => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [userToken, setUserToken] = React.useState(null);
 
+  const authContext = React.useMemo(() => ({
+    signIn: () => {
+      setUserToken("rfdsa");
+      setIsLoading(flase);
+    },
+    signOut: () => {
+      setUserToken(null);
+      setIsLoading(flase);
+    },
+    signUp: () => {
+      setUserToken("rfdsa");
+      setIsLoading(flase);
+    },
+  }));
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }),
+      1000;
+  }, []);
+
   if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -17,6 +39,7 @@ const App = () => {
       </View>
     );
   }
+
   return (
     <NavigationContainer>
       <RootStackScreen />
