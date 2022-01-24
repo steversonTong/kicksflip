dc2c87df9b183788248e55d363088bde86036bc1
Steve Tong <stevetong53@yahoo.com>
2022-01-23 19:36:35 -0800

    created login reducer

--- App.js
+++ App.js
@@ -11,23 +11,71 @@ import { AuthContext } from "./components/context";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [userToken, setUserToken] = React.useState(null);
+  // const [isLoading, setIsLoading] = React.useState(true);
+  // const [userToken, setUserToken] = React.useState(null);
 
-  const authContext = React.useMemo(() => ({
-    signIn: () => {
-      setUserToken("rfdsa");
-      setIsLoading(false);
-    },
-    signOut: () => {
-      setUserToken(null);
-      setIsLoading(false);
-    },
-    signUp: () => {
-      setUserToken("rfdsa");
-      setIsLoading(false);
-    },
-  }));
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    // userEmail: null,
+    userToken: null,
+  };
+
+  const loginReducer = (prevState, action) => {
+    switch (action.type) {
+      case "RETRIEVE_TOKEN":
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case "LOGIN":
+        return {
+          ...prevState,
+          userName: action.id,
+          // userEmail: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case "LOGOUT":
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
+        };
+      case "REGISTER":
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+    }
+  };
+
+  const [loginState, dispatch] = React.useReducer(
+    loginReducer,
+    initialLoginState
+  );
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: () => {
+        // setUserToken("rfdsa");
+        // setIsLoading(false);
+      },
+      signOut: () => {
+        // setUserToken(null);
+        // setIsLoading(false);
+      },
+      signUp: () => {
+        // setUserToken("rfdsa");
+        // setIsLoading(false);
+      },
+    }),
+    []
+  );
 
   useEffect(() => {
     setTimeout(() => {
