b3549c3d708cc099e0ad98c27a9b21ee1a623847
Steve Tong <stevetong53@yahoo.com>
2022-01-23 19:22:13 -0800

    sign in and sign out button takes from splash page to home screen and vise versa

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,22 +1,106 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView, Platform } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { AuthContext } from "../components/context";
 
 const AccountScreen = () => {
+  const { signOut } = React.useContext(AuthContext);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{ color: "white" }}>Account Screen</Text>
+      <View style={styles.button}>
+        <TouchableOpacity
+          onPress={() => {
+            signOut();
+          }}
+        >
+          <Text style={{ color: "white" }}>
+            Log Out <Icon name="exit-to-app" color="white" size={20} />
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
+
+export default AccountScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#4B4B4B",
     alignItems: "center",
+    paddingHorizontal: 20,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  text_header: {
+    color: "#556B2F",
+    alignItems: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  body: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 40,
+    paddingVertical: 50,
+  },
+  text_basic: {
+    color: "#4B4B4B",
+    alignItems: "flex-start",
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#4B4B4B",
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
+  },
+  text_input: {
+    flex: 1,
+    marginTop: Platform.OS === "ios" ? 0 : -12,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+  errorMsg: {
+    color: "#FF0000",
+    fontSize: 14,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: "#556B2F",
+    borderRadius: 10,
+    height: 40,
+    width: 250,
+  },
+  signIn: {
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
-
-export default AccountScreen;
