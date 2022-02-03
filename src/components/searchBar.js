2a2a6e980476b7ce49c724f0b03f57112a9ae9d8
Steve Tong <stevetong53@yahoo.com>
2022-02-03 14:55:22 -0800

    added search bar, error with clicking on search bar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,28 +1,94 @@
-import React, { useState } from "react";
-import { View, SearchBar, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet, TextInput, View, Keyboard, Button } from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import Entypo from "react-native-vector-icons/Entypo";
 
-const searchBar = () => {
-  const [search, setSearch] = useState("");
-
-  const updateSearch = (search) => {
-    setSearch(search);
-  };
-
+const SearchBar = (props) => {
   return (
-    <View style={styles.view}>
-      <SearchBar
-        placeholder="Type Here..."
-        onChangeText={updateSearch}
-        value={search}
-      />
+    <View style={styles.container}>
+      <View
+        style={
+          !props.clicked
+            ? styles.searchBar__unclicked
+            : styles.searchBar__clicked
+        }
+      >
+        {/* search Icon */}
+        <Feather
+          name="search"
+          size={20}
+          color="black"
+          style={{ marginLeft: 1 }}
+        />
+        {/* Input field */}
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={props.searchPhrase}
+          onChangeText={props.setSearchPhrase}
+          onFocus={() => {
+            props.setClicked(true);
+          }}
+        />
+        {/* cross Icon, depending on whether the search bar is clicked or not */}
+        {props.clicked && (
+          <Entypo
+            name="cross"
+            size={20}
+            color="black"
+            style={{ padding: 1 }}
+            onPress={() => {
+              props.setSearchPhrase("");
+            }}
+          />
+        )}
+      </View>
+      {/* cancel button, depending on whether the search bar is clicked or not */}
+      {props.clicked && (
+        <View>
+          <Button
+            title="Cancel"
+            onPress={() => {
+              Keyboard.dismiss();
+              props.setClicked(false);
+            }}
+          ></Button>
+        </View>
+      )}
     </View>
   );
 };
+export default SearchBar;
 
+// styles
 const styles = StyleSheet.create({
-  view: {
-    margin: 10,
+  container: {
+    margin: 15,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    flexDirection: "row",
+    width: "90%",
+  },
+  searchBar__unclicked: {
+    padding: 10,
+    flexDirection: "row",
+    width: "95%",
+    backgroundColor: "#d9dbda",
+    borderRadius: 15,
+    alignItems: "center",
+  },
+  searchBar__clicked: {
+    padding: 10,
+    flexDirection: "row",
+    width: "80%",
+    backgroundColor: "#d9dbda",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  input: {
+    fontSize: 20,
+    marginLeft: 10,
+    width: "90%",
   },
 });
-
-export default searchBar;
