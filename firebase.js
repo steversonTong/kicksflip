a2380ca99975e23d6608a57cf14b89fc9e4c5938
Steve Tong <stevetong53@yahoo.com>
2022-02-03 16:51:55 -0800

    change firebase auth user

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBBaHuYCfOzayytTG2uUGUt6o88JBcu4ss",
-  authDomain: "kicksflip-auth.firebaseapp.com",
-  projectId: "kicksflip-auth",
-  storageBucket: "kicksflip-auth.appspot.com",
-  messagingSenderId: "1082201278771",
-  appId: "1:1082201278771:web:3bb8092a8f8b6f80fb9646",
+  apiKey: "AIzaSyAdsUVa7eeLQQgdlHagpKL3EgGXcFsx7mA",
+  authDomain: "kicksflip-191c8.firebaseapp.com",
+  projectId: "kicksflip-191c8",
+  storageBucket: "kicksflip-191c8.appspot.com",
+  messagingSenderId: "1042372696104",
+  appId: "1:1042372696104:web:de86e014a958de0fa2fd1d",
 };
 
 // Initialize Firebase
